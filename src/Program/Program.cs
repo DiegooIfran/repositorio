3d55b1778fdb801f4ea1c3b93c5ccd64236160b6
Luutyy <lautaramirezlopez@gmail.com>
2025-08-18 08:50:11 -0300

    Prueba

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,11 +3,34 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+public class Addition
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
 // Esta clase implementa la operación division
 public class Division
 {
     public static double Divide(int a, int b)
     {
         return 0;
+    }
+}
+// Esta clase implementa la operación substraction
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
+// Esta clase implementa la operación multiplicación
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
     }
 }
